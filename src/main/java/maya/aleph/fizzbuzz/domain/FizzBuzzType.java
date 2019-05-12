1c0563ccd561704107b78ac8300aa37dcb7bf6c3
Maya <maya.up7777@gmail.com>
2019-05-12 17:51:06 -0400

    Override enum toString method by lower casing enums values.

--- src/main/java/maya/aleph/fizzbuzz/domain/FizzBuzzType.java
+++ src/main/java/maya/aleph/fizzbuzz/domain/FizzBuzzType.java
@@ -1,5 +1,10 @@
 package maya.aleph.fizzbuzz.domain;
 
 public enum FizzBuzzType {
-    NUMBER, BUZZ, FIZZBUZZ, FIZZ
+    NUMBER, BUZZ, FIZZBUZZ, FIZZ;
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
 }
