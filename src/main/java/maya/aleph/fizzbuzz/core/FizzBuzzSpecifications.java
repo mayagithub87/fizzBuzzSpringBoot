7cd375eb9621307317491073d0158fa530d3840e
Maya <maya.up7777@gmail.com>
2019-05-14 17:09:05 -0400

    Make "final" static fields and create private default constructor in FizzBuzzSpecifications class. These suggestions were made by Sonar Lint plugin.

--- src/main/java/maya/aleph/fizzbuzz/core/FizzBuzzSpecifications.java
+++ src/main/java/maya/aleph/fizzbuzz/core/FizzBuzzSpecifications.java
@@ -4,12 +4,12 @@ import java.util.function.IntPredicate;
 
 public class FizzBuzzSpecifications {
 
-    public static IntPredicate isFizz = number -> number % 3 == 0 && number % 5 != 0;
+    public static final IntPredicate isFizz = number -> number % 3 == 0 && number % 5 != 0;
+    public static final IntPredicate isBuzz = number -> number % 3 != 0 && number % 5 == 0;
+    public static final IntPredicate isFizzBuzz = number -> number % 3 == 0 && number % 5 == 0;
+    public static final IntPredicate isNotFizzBuzz = number -> number % 3 != 0 && number % 5 != 0;
 
-    public static IntPredicate isBuzz = number -> number % 3 != 0 && number % 5 == 0;
-
-    public static IntPredicate isFizzBuzz = number -> number % 3 == 0 && number % 5 == 0;
-
-    public static IntPredicate isNotFizzBuzz = number -> number % 3 != 0 && number % 5 != 0;
+    private FizzBuzzSpecifications() {
+    }
 
 }
